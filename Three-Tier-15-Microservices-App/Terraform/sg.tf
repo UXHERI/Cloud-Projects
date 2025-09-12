310a9d3d8ba4f8341277ddae788193b490d74199
Uzair Khan <74897912+UXHERI@users.noreply.github.com>
2025-09-12 10:23:59 +0500

    Create sg.tf

---
+++ Three-Tier-15-Microservices-App/Terraform/sg.tf
@@ -0,0 +1,184 @@
+# Jump Server SG
+resource "aws_security_group" "jump_server" {
+  name        = "jump-server-sg"
+  description = "Security group for jump server"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Jump-Server"
+  }
+}
+
+# Web Frontend SG
+resource "aws_security_group" "web_frontend_alb" {
+  name        = "web-frontend-alb-sg"
+  description = "Security group for web ALB"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-frontend-alb"
+  }
+}
+
+# Web Server SG
+resource "aws_security_group" "web_srv" {
+  name        = "web-srv-sg"
+  description = "Security group for web servers"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_frontend_alb.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jump_server.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-srv-sg"
+  }
+}
+
+# App Internal SG
+resource "aws_security_group" "app_internal_alb" {
+  name        = "app-internal-alb-sg"
+  description = "Security group for internal ALB"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_srv.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-Internal-alb-sg"
+  }
+}
+
+# App Server SG
+resource "aws_security_group" "app_srv" {
+  name        = "app-srv-sg"
+  description = "Security group for application servers"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port       = 4000
+    to_port         = 4000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_internal_alb.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jump_server.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-Srv-sg"
+  }
+}
+
+# DB Server SG
+resource "aws_security_group" "db_srv" {
+  name        = "db-srv-sg"
+  description = "Security group for database"
+  vpc_id      = aws_vpc.three_tier_vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_srv.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jump_server.id]
+  }
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jump_server.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "DB-srv"
+  }
+}
