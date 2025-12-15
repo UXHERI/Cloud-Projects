01473ec33b9e596de236d3ea088608a1c820f938
Uzair Khan <74897912+UXHERI@users.noreply.github.com>
2025-12-15 19:21:35 +0500

    Create security-groups.tf

---
+++ Same-Region-VPC-Peering/Terraform/security-groups.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "allow_ssh_icmp_1a" {
+  vpc_id = aws_vpc.vpc_1a.id
+  name   = "allow-ssh-icmp-1a"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_ssh_icmp_1b" {
+  vpc_id = aws_vpc.vpc_1b.id
+  name   = "allow-ssh-icmp-1b"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
